0d5e347f7a0d70a1830536d1ee7052ce93120a9c
hjyeon-n <hyunjy25@gmail.com>
2024-06-23 19:05:28 +0900

    feat: 전역 예외 핸들러 추가

---
+++ src/main/java/kr/co/hanbit/product/management/presentation/ErrorMessage.java
@@ -0,0 +1,14 @@
+package kr.co.hanbit.product.management.presentation;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class ErrorMessage {
+    private List<String> errors;
+
+    public ErrorMessage(List<String> errors) {
+        this.errors = errors;
+    }
+}
